8f1f3d21de875435949aea8d8952609133cb5d5c
Zombox <ghostzb@outlook.com>
2025-03-19 20:17:06 +0700

    first commit

---
+++ constraint/phoneVerification.ts
@@ -0,0 +1,11 @@
+export type PhoneVerificationType = {
+  phoneNumber: string
+  pin: string
+}
+
+export const phoneVerificationData:PhoneVerificationType[] = [
+  {
+    phoneNumber: "0943135555",
+    pin: "147369"
+  }
+]
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
