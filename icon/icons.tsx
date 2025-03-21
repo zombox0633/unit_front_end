bf8f40c093a34b59dff76386534288edfcda51fc
Zombox <ghostzb@outlook.com>
2025-03-22 00:30:09 +0700

    edit ChevronForwardIcon color

--- icon/icons.tsx
+++ icon/icons.tsx
@@ -30,7 +30,7 @@ export const ChevronForwardIcon: React.FC = () => (
     height="24px"
     viewBox="0 -960 960 960"
     width="24px"
-    fill="var(--icon-color)"
+    fill="var(--color-cool-gray)"
   >
     <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
   </svg>
@@ -42,7 +42,7 @@ export const BackspaceIcon: React.FC = () => (
     height="24px"
     viewBox="0 -960 960 960"
     width="24px"
-    fill="var(--icon-color)"
+    fill="var(--color-pigment-green)"
   >
     <path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z" />
   </svg>
