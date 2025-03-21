3aab87243526a28f3351eca469b9fc2f2464219d
Zombox <ghostzb@outlook.com>
2025-03-21 22:13:08 +0700

    create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import styles from "./page.module.css";
+"use client"
+import { useRouter } from "next/navigation"
+import styles from "./page.module.css"
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className={styles.container}>
-      <h1>Test</h1>
+      <header>
+        <h1>Unit Co Ltd - Frontend Exam</h1>
+      </header>
+      <button type="button" onClick={() => router.push("/phone")}>
+        ยืนยันหมายเลขโทรศัพท์
+      </button>
     </div>
-  );
+  )
 }
